513936a3d55dfdf21c78f4ae417ac5c3d69f8bae
Abhishek Chakraborty <eofsentinel@outlook.com>
2021-11-04 13:57:15 +0530

    Added git pull default config

--- Git/global-config.sh
+++ Git/global-config.sh
@@ -1,3 +1,4 @@
 git config --global core.editor vim
 git config --global user.name "Abhishek Chakraborty"
 git config --global user.email "eofsentinel@outlook.com"
+git config --global pull.rebase true
